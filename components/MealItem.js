7905e3a1e09f26299809b1a1fa0bc50ae9b7682e
zeinab-zeitoun <zeinabzeitoun.98@gmail.com>
2022-04-11 15:15:48 +1000

    implemented meals categories details

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,9 +1,22 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable, Image } from "react-native";
 
-function MealItem({ title }) {
+function MealItem({ title, imageUrl, duration, complexity, affordability }) {
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        android_disableSound={{ color: "#ccc" }}
+        style={({ pressed }) => pressed && styles.buttonPressed}
+      >
+        <View>
+          <Image style={styles.image} source={{ uri: imageUrl }} />
+          <Text style={styles.title}>{title}</Text>
+        </View>
+        <View style={styles.details}>
+          <Text style={styles.detailsItem}>{duration}m</Text>
+          <Text style={styles.detailsItem}>{complexity.toUpperCase()}</Text>
+          <Text style={styles.detailsItem}>{affordability.toUpperCase()}</Text>
+        </View>
+      </Pressable>
     </View>
   );
 }
@@ -11,5 +24,33 @@ function MealItem({ title }) {
 export default MealItem;
 
 const styles = StyleSheet.create({
- 
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailsItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+  buttonPressed: {
+    opacity: 0.25,
+  },
 });
