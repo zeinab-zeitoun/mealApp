8bbf8a8fea6749a573620a53acc7d533dec1cfd6
zeinab-zeitoun <zeinabzeitoun.98@gmail.com>
2022-04-12 11:44:29 +1000

    configuring stack navigation

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Button, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen}  />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen}  />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#351401" },
+            headerTintColor: "white",
+            contentStyle: { backgroundColor: "#3f2f25" },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{
+              title: "Meals Categories",
+            }}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            options={{
+              title: "Meals Overview",
+              headerRight: () => {
+                return <Button title="tap me" />;
+              },
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,14 +1,25 @@
 import { View, FlatList, StyleSheet } from "react-native";
+import { useLayoutEffect } from "react";
 import { useRoute } from "@react-navigation/native";
-import { MEALS } from "../data/dummy-data";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import MealItem from "../components/MealItem";
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
 
   const displayMeals = MEALS.filter((meal) => {
     return meal.categoryIds.indexOf(catId) >= 0;
   });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, CATEGORIES]);
 
   function renderMealItem(itemData) {
     const item = itemData.item;
@@ -20,9 +31,7 @@ function MealsOverviewScreen({ route }) {
       duration: item.duration,
     };
 
-    return (
-      <MealItem {...mealItemProps}/>
-    );
+    return <MealItem {...mealItemProps} />;
   }
 
   return (
